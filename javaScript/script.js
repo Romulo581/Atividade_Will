fa0eb384acf17a4f8915f80c40caa10d8b0bb1d4
Rômulo César <Romulo34007126@edu.df.senac.br>
2024-10-16 19:48:35 -0300

    novo

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,3 +1,21 @@
-function showMessage(){
+function showMessage() {
     alert('Olá, esta é uma interação com JavaScript')
+}
+function mudarColor() {
+    var text1 = document.getElementById('text').style.color;
+    if (text1 === 'red') {
+        console.log(text1)
+        document.getElementById('text').style.color = 'blue';
+    } else {
+        document.getElementById('text').style.color = 'red';
+    }
+}
+const alt2 = document.getElementById('text').textContent;
+function textAltere() {
+    var alt = document.getElementById('text').textContent;
+    if (alt !== "Alterado") {
+        document.getElementById('text').textContent = "Alterado";
+    } else {
+        document.getElementById('text').textContent = alt2;
+    }
 }
