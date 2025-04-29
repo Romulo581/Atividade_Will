245ded7ddfefddbf09f2042c9ac6c0d13b3e9418
Romulo581 <151069888+Romulo581@users.noreply.github.com>
2025-04-29 10:11:17 -0300

    add card4

--- javaScript_t/script.js
+++ javaScript_t/script.js
@@ -6,10 +6,14 @@ window.onload = function(){
     const cards2 = document.getElementById("cards_efx2");
     setTimeout(() =>{
         cards2.classList.add("show2");
-    },105);
+    },95);
     const cards3 = document.getElementById("cards_efx3");
     setTimeout(() =>{
         cards3.classList.add("show3");
-    },110);
+    },92);
+    const cards4 = document.getElementById("cards_efx4");
+    setTimeout(() =>{
+        cards4.classList.add("show4");
+    },95);
 
 }
