b1a994ebb4c3867e017e3b4724078cfc864ed6ae
Romulo581 <151069888+Romulo581@users.noreply.github.com>
2025-04-29 09:02:22 -0300

    dev:romulo_trabalho

---
+++ javaScript_t/script.js
@@ -0,0 +1,15 @@
+window.onload = function(){
+    const cards1 = document.getElementById("cards_efx1");
+    setTimeout(() =>{
+        cards1.classList.add("show1");
+    },100);
+    const cards2 = document.getElementById("cards_efx2");
+    setTimeout(() =>{
+        cards2.classList.add("show2");
+    },108);
+    const cards3 = document.getElementById("cards_efx3");
+    setTimeout(() =>{
+        cards3.classList.add("show3");
+    },110);
+
+}
